860f8af9c1434bb55ce09f372d0b790e6b2685c4
csnwc <Neill.Campbell@bristol.ac.uk>
2025-10-22 11:57:02 +0100

    Moved variables inside #if

--- Code/Week5/TentsTrees/driver.c
+++ Code/Week5/TentsTrees/driver.c
@@ -4,18 +4,18 @@ int main(void)
 {
    test();
 
-   board b;
-   char s[BSIZE*(BSIZE+1)];
-   // FAIL : Empty string
-   assert(inputboard("", "111111", "222222", &b) ==false);
 
    // We only compile these lines if BSIZE has been #defined to be 6
    #if BSIZE == 6
+      char s[BSIZE*(BSIZE+1)];
+      board b;
+      // FAIL : Empty string
+      assert(inputboard("", "111111", "222222", &b) ==false);
       // String too long (extra unknown on 5th line)
       assert(inputboard("*    *|    * |      |  *  *|       | **   ","120121", "201112", &b)==false);
       // Invalid square ('N')
       assert(inputboard("*    *|    * |  N   |  *  *|      | **   ", "120121", "201112", &b)==false);
-      // Only unknown or tree squares allowed on input, not grass
+      // Only unknown or tent squares allowed on input, not grass
       assert(inputboard("*    *|    * | ...  |  *  *|      | **   ", "120121", "201112", &b)==false);
       // Total tens per row (6), not same as per column (7)
       assert(inputboard("*    *|    * |      |  *  *|      | **   ", "120121", "201111", &b)==false);
@@ -47,6 +47,8 @@ int main(void)
    #endif
 
    #if BSIZE == 4
+      char s[BSIZE*(BSIZE+1)];
+      board b;
       assert(inputboard("*  *|    |*   |   *", "1111", "1102", &b)==true);
       assert(solve(&b)==true);
       assert(board2str(b,s));
